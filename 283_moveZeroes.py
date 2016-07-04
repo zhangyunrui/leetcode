000be05cd5b48ab42d8cbd75c3a2297099d56ebd
zhangyunrui <635798274@qq.com>
2016-07-04 22:21:01 +0800

    movezero

--- 283_moveZeroes.py
+++ 283_moveZeroes.py
@@ -5,4 +5,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        nums.sort(key=lambda x: 1 if x == 0 else 0)
+        return nums
 
+print Solution().moveZeroes([1,5,3,0,9,0])
